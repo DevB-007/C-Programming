b9a8992e7006e087633f137f95420829eb157cef
DevB-007 <devbumtariya.ce@gmail.com>
2025-07-18 16:10:04 +0530

    Add files via upload

---
+++ Practical 1/Programme in turboc3/Dev Bumtariya/SECOND.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+printf("HELLO EVERYONE\n");
+printf("Myself Dev B.\n");
+printf("I'm a student at CHARUSAT university.\n");
+getch();
+}
