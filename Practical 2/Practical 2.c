da79d3338c16deb832ae9077b2a8f5643f741237
DevB-007 <devbumtariya.ce@gmail.com>
2025-07-21 15:32:29 +0530

    Add files via upload

---
+++ Practical 2/Practical 2.c
@@ -0,0 +1,11 @@
+//This program denotes the calculation for area and perimeter.
+#include<stdio.h>
+int main()
+{
+    int len=70,brd=90,area,peri;
+    area=len*brd;
+    printf("The area of park is: %d\n",area);
+    peri=2*(len+brd);
+    printf("The perimeter of the park: %d\n",peri);
+    printf("This program is developed by Dev Bumtariya.");
+}
