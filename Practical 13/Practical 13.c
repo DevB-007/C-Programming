a85efee71f93e414d2fac14a502cd4427993fe81
DevB-007 <devbumtariya.ce@gmail.com>
2025-08-18 15:38:39 +0530

    Add files via upload

---
+++ Practical 13/Practical 13.c
@@ -0,0 +1,14 @@
+/*Program to display the water levels of the automatic refilling tank (at the rate of 10 liters per minutes).*/
+#include<stdio.h>
+int main()
+{
+    for(int x=1;x<=9;x++)
+    {
+        printf("Current water level: %d liters",x*10);
+        printf("\n");
+        sleep(1);
+    }
+    printf("Tank is full.");
+    printf("\nThis program is developed by DEV BUMTARIYA.");
+    return 0;
+}
