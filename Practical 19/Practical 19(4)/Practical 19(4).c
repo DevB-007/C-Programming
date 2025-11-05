52e6dd5b00b7d25734d33f3048d991ab6c55968c
DevB-007 <devbumtariya.ce@gmail.com>
2025-11-05 17:29:29 +0530

    Add files via upload

---
+++ Practical 19/Practical 19(4)/Practical 19(4).c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,y,x;
+    printf("Enter any number: ");
+    scanf("%d",&n);
+    x=1;
+    y=n-1;
+    for(int i=1; i<=2*n-1; i++)
+    {
+        if(i<=n)
+        {
+            for(int j=y; j>0; j--)
+            {
+                printf(" ");
+            }
+            for(int k=1; k<=x; k++)
+            {
+                printf("%c",64+k);
+            }
+            for(int h=x-1; h>0; h--)
+            {
+                printf("%c",64+h);
+            }
+            x++;
+            y--;
+        }
+        else
+        {
+            for(int j=1; j<=i-n; j++)
+            {
+                printf(" ");
+            }
+            for(int k=1; k<=n+y; k++)
+            {
+                printf("%c",64+k);
+            }
+            y--;
+            for(int h=x-3; h>0; h--)
+            {
+                printf("%c",64+h);
+            }
+            x--;
+        }
+        printf("\n");
+    }
+    return 0;
+}
